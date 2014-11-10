e66d77c604bca9192e0a8cc515024b02827842e7
Tim Veil <tjveil@gmail.com>
2014-11-10 16:24:28 -0500

    adding spring

--- src/main/java/com/github/randerzander/config/AppConfig.java
+++ src/main/java/com/github/randerzander/config/AppConfig.java
@@ -2,14 +2,12 @@ package com.github.randerzander.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
-
 
     @Bean
     public DataSource buildDataSource() {
