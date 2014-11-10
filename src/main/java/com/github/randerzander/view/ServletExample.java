a3ed07f647c7f94704556bceae7ba57358dfe3f7
Tim Veil <tjveil@gmail.com>
2014-11-10 16:48:20 -0500

    adding spring

--- src/main/java/com/github/randerzander/view/ServletExample.java
+++ src/main/java/com/github/randerzander/view/ServletExample.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServlet;
 import javax.sql.DataSource;
