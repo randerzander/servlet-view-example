829806a0870d5198abd5f56a0454ac3525164b20
Randy Gelhausen <rgelhau@gmail.com>
2014-11-13 11:59:53 -0500

    Added pretty collapsers, results in a table, Save as CSV link

--- src/main/resources/ui/index.js
+++ src/main/resources/ui/index.js
@@ -1,15 +1,45 @@
-function submit(){
+$('#submit').on('click', function(){
+  var $btn = $(this).button('loading');
   $.ajax({
     type: 'POST',
     url: 'services',
     data: $('#txtquery').val(),
     success: function(data){
       var result = JSON.parse(data);
-      $('#result').text(data);
+      var csv = "data:text/csv;charset=utf-8,";
+
+      //Append headers
+      csv += result.columns.join(',') + '\n';
+      $.each(result.columns, function(i, v){
+        $('#rows>thead>tr').append('<th>' + v + '</th>');
+      });
+      //Append rows
+      $.each(result.result, function(i, v){
+        //Append cells
+        csv += v.join(',') + '\n';
+        var row = '<tr>';
+        $.each(v, function(index, val){
+          row += '<td>' + val + '</td>';
+        });
+        $('#rows>tbody').append(row + '</tr>');
+      });
+      $btn.button('reset');
+
+      //Make csv results downloadable
+      var link = document.createElement('a');
+      link.setAttribute('href', encodeURI(csv));
+      link.setAttribute('download', 'result.csv');
+      link.innerHTML='Save CSV';
+      $('#csvlink').removeClass('hidden').append(link);
+
+      //Update panes collapse state
+      $('#collapseOne').removeClass('in').addClass('collapsing');
+      $('#collapseTwo').addClass('in');
     }
   });
-}
+});
 
+/* For future integration with datatables or dynatable */
 function toRecords(result){
   var data = [];
   $.each(result.result, function(i, v){
@@ -19,4 +49,5 @@ function toRecords(result){
     })
     data.push(record);
   });
+  return data;
 }
